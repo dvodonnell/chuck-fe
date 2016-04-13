f3c6c9a7087fdcd9db1c70e29cdec8b26e12e76b
Daniel O'Donnell <dan@dvoc.ca>
2016-04-13 15:08:53 -0400

    base stuff

---
+++ src/Chuck.js
@@ -0,0 +1,72 @@
+export default {
+
+    define : function(config) {
+
+        return (function(_config){
+
+            var App = function(innerConfig) {
+
+                this.config = innerConfig;
+
+                this._container = [];
+
+            };
+
+            App.prototype = {
+
+                service : _config.service,
+                source : _config.source,
+                store : _config.store,
+
+                renderer : _config.renderer,
+                runner : _config.runner,
+
+                _validateConfig : function(config) {
+
+                },
+
+                _parseConfig : function(config) {
+
+
+                },
+
+                _instantiate : function() {
+
+
+
+                },
+
+                _buildAppPayload : function() {
+                    return {
+                        store : this.store,
+                        service : this.service
+                    }
+                },
+
+                render : function() {
+                    for (var i = 0; i < this._container.length; i++) {
+                        this.renderer(this._buildAppPayload(), this._container[i]);
+                    }
+                },
+
+                renderStatic : function() {
+                    return this.renderer(this._buildAppPayload(), null, true);
+                },
+
+                mount : function(el) {
+                    this._container.push(el);
+                },
+
+                run : function(initialState) {
+                    this.runner(this._buildAppPayload(), initialState);
+                }
+
+            };
+
+            return App;
+
+        })(config);
+
+    }
+
+};
